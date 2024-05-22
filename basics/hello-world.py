3370d38451ada320afc77061505ee778f774abe3
ssmeherk <mehernivas@gmail.com>
2024-05-22 12:37:01 +0530

    Adding hello-world.py

---
+++ basics/hello-world.py
@@ -0,0 +1,6 @@
+#####################################################################
+## Author: ssmeherk
+## Date: 22.05.2024
+## Version: V1
+## Purpose: To write a hello world python code and check into git repo
+######################################################################
